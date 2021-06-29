4971f2594a42d8f9240ca812d648f3e2eceedde9
tatidan <t.danyliuk@gmail.com>
2021-06-29 18:04:27 +0300

    created project basics

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import './sass/main.scss';
+
+// const colors = [
+//   '#FFFFFF',
+//   '#2196F3',
+//   '#4CAF50',
+//   '#FF9800',
+//   '#009688',
+//   '#795548',
+// ];
+
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+const button = document.querySelector('button[data-action="start]');
+console.log(button);
+console.log("тест связи");
+
+window.addEventListener('click');
+
+// const startBtn = document.querySelector('[data-action="start"]');
+// startBtn.addEventListener('click');
+// //const startBtn = document.querySelector('button data-action="start"');
+// console.log(startBtn);
+// console.log('click');
+
+
+//var el = document.querySelector("div.user-panel.main input[name=login]");
+
+//Напиши скрипт, который после нажатия кнопки Start,
+//раз в секунду меняет цвет фона body на случайное значение из массива
+//используя инлайн - стиль.При нажатии на кнопку Stop,
+//изменение цвета фона должно останавливаться.
+
+//Учти, на кнопку Start можно нажать бесконечное количество раз.
+//Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
+
+//Для генерации случайного числа(индекс элемента массива цветов),
+//используй функцию randomIntegerFromInterval.
